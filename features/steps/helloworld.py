f96197560fb48fa6ceaf41fd8b58d8cd33ae49bb
John Quinn <quinnjc@g.cofc.edu>
2020-03-07 19:14:12 +0000

    lots of comments, some bug fixes

--- features/steps/helloworld.py
+++ features/steps/helloworld.py
@@ -1,28 +1,71 @@
 from behave import *
-#from dogtail import rawinput
-## issue: python3-dogtail is only in debian-testing
-## this distro is running debian stable
-## there is no python2.7 behave, so we're stuck in python3
+from dogtail.procedural import *
 import subprocess
+from subprocess import Popen, PIPE, STDOUT
+import os
+import tempfile
 
+TEST_TEXT = "Hello world!"
 
 @given('we have nano installed')
 def step_impl(context):
     pass
 
-@when('we open nano')
+@when('we open mousepad')
 def step_impl(context):
-    subprocess.call("nano")
+    #
+    # Two major options recognized for user input into a subprocess are recognized:
+    # First, there is the option of capturing stdin from the parent process,
+    # and redirecting it to the stdin of the child process.
+    #
+    # Secondly, there is the option of forking a new process completely, focusing
+    # that process, and inputing stdin directly into that.
+    #
+    # this implementation uses the second option with Popen being a lowlevel
+    # interface, accessed by the subprocess.run. Care must be taken to ensure
+    # the created process actually exits.
+    #
+    
+    mousepadproc = subprocess.Popen(["mousepad", "&"])
+    context.testProcess=mousepadproc
+    pass
 
 @when('we write hello world')
 def step_impl(context):
-    #dogtail.rawinput.typeText("hello world")
+    focus.application("mousepad")
+    type(TEST_TEXT)
+    pass
 
 @when('we quit and save the file')
 def step_impl(context):
-    #dogtail.rawinput.keyCombo('<Control>x')
-    #dogtail.rawinput.pressKey('y')
+    context.dirname=tempfile.mkdtemp()
+    context.filename = os.path.join(context.dirname, "hello.txt")
+    keyCombo("<ctrl>s")
+    type(context.filename)
+    focus.widget("Save As")
+    click.button("Save")
+
+    # wait a little for it to finish
+    try:
+        context.wait(5)
+
+    except subprocess.TimeoutExpired:
+     # kills it, -9 style if it takes too long
+        context.testProcess.kill()
+    pass
 
 @then('the file will exist')
 def step_impl(context):
-    assert context.failed is False
+    # checks for the file
+    if os.path.isfile(context.filename):
+        pass
+    else:
+        # TODO (for John) throw an error
+        pass
+
+@then('the returncode was zero')
+def step_impl(context):
+    #cleans up
+    os.remove(context.filename)
+    os.rmdir(context.filename)
+    context.completedProcess.check_returncode() #throws error if nonzero
--- features/steps/terminalapplications.py
+++ features/steps/terminalapplications.py
@@ -13,14 +13,16 @@ import subprocess
 DEBUG_MODE = True
 
 TEST_TEXT = "Hello world!"
-
+    
 @given('we are testing nano and xterm')
 def step_impl(context):
+    # applications placed here can be increased, just don't spell it wrong
     context.testApplications = ["nano","xterm"]
     
 @given('the application(s) to be tested are installed')
 def step_impl(context):
     # execute dpkg on each application to check if it's installed
+    
     for application in context.testApplications :
         # execute, capture output and error
         output = subprocess.check_output(["dpkg", "--status", application], encoding='ascii')
@@ -36,33 +38,54 @@ def step_impl(context):
 
 @when('we open a new xterm terminal')
 def step_impl(context):
+
+    #
+    # TODO: use similar Popen structure as in helloworld.py to avoid process hang,
+    # handle exit scenarios properly
+    #
+    # open in a separate terminal window
+    #
     context.completedProcess=subprocess.call(["xterm", "&"], shell=True) # behave hangs here until this process returns
     pass
     
 @when('we open nano')
 def step_impl(context):
-    # the options on this are interesting.
-    # shell=True executes the command in a true subshell, with corresponding security
-    # implications.
     #
-    # Two major options recognized for user input into a subprocess are recognized:
-    # First, there is the option of capturing stdin from the parent process,
-    # and redirecting it to the stdin of the child process.
+    # TODO: focus xterm window, type nano into that window, press enter
     #
-    # Secondly, there is the option of forking a new process completely, focusing
-    # that process, and inputing stdin directly into that.
+    # Don't worry too much about handling errors, the test simply fails if an exception throws
+    # since xterm is not used by default on this machine, it should be unique
+    # HOWEVER we will need to test the xfce terminal too, which presents problems
     #
-    # This implementation uses the second option.
+    
     pass
 
 
 @when('we write some in nano and save it to a file')
 def step_impl(context):
-    input()
+    #
+    # TODO: grab focus of nano running in xterm  not already grabbed
+    # type into nano
+    # send <ctrl>+x and then y to save and quit, test of course manually
+    #
+    # ALSO, these will need to be written into behave, since they don't work I haven't written them in
+    # simply add "When we write some in nano and save it to a file" - spelling matters
+    # if you see the word "And" in behave, it's just copying the last line's first word, more readable,
+    # you can do that as well
+    #
+    # when should be test actions, then should be the oracle section
+    #
+    
     type(TEST_TEXT)
     context.oracleText = TEST_TEXT
     pass
 
 @then('that file will have the text we wrote')
 def step_impl(context):
+    #
+    # TODO: the whole thing, I can do this if you don't do it, just
+    # read the file from python, and check against - context.test_text, or whatever you use
+    # the context object is passed from step to step, so use it to hold variables, objects, etc
+    #
+    
     assert context.failed is False
